d22d0b8427cfd740750bcd41f7ce685229154f46
Matias <matias.mujicaq@gmail.com>
2022-11-09 22:10:29 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from "./Componentes/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <NavBar />
+      </main>
+    </div>
+  );
+}
+
+export default App;
